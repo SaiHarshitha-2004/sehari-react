ddf3a196d40591bcc96fdfd3781603cab1845901
SaiHarshitha-2004 <108074739+SaiHarshitha-2004@users.noreply.github.com>
2024-07-03 23:09:24 +0530

    Update App.jsx

--- Client/App.jsx
+++ Client/App.jsx
@@ -1,43 +1,3 @@
-// import React from 'react'
-// import Home from '../Client/src/pages/Home.jsx'
-// import Services from '../Client/src/pages/Services.jsx';
-// import Login from '../Client/src/pages/Login.jsx';
-// import {createBrowserRouter , BrowserRouter, Routes, Route } from "react-router-dom";
-// import SignUp from '../Client/src/pages/SignUp.jsx'; 
-// import HereMap from '../Client/src/Maps/HereMap.jsx';
-// import DisplayVenues from "../Client/src/HomeComponents/DisplayVenues.jsx";
-// import {Animations} from '../Client/src/Data/Animations.jsx';
-// import VenueCard from '../Client/src/HomeComponents/VenueCard.jsx';
-// import NavBar from '../Client/src/pages/NavBar.jsx';
-// import Error from '../Client/src/pages/Error.jsx';
-
-// const App = () => {
-  
-//   return (
-//     <>
-//     <NavBar />
-//     <BrowserRouter> 
-//       <Routes>
-//         <Route path="/" element={ <Home />} />
-//         <Route path="/services" element={ <Services /> } />
-//         <Route path="/login" element={ <Login /> } />
-//         <Route path="/signup" element={ <SignUp /> } />
-
-//         <Route path="/map" element={ <HereMap /> } />
-//         <Route path="/displayallvenues" element={ <DisplayVenues /> } />
-//         <Route path="/loader" element={ <Animations /> } />
-//         <Route path="/venuecard" element={ <VenueCard /> } />
-//         <Route path="/*" element={ <Error /> } />
-
-//       </Routes>
-//     </BrowserRouter>
-//     </>
-//   )
-// }
-
-// export default App
-
-
 import React, { createContext} from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from '../Client/src/pages/Home.jsx';
@@ -50,7 +10,6 @@ import { Animations } from '../Client/src/Data/Animations.jsx';
 import VenueCard from '../Client/src/HomeComponents/VenueCard.jsx';
 import NavBar from '../Client/src/pages/NavBar.jsx';
 import Error from '../Client/src/pages/Error.jsx';
-import "react-toastify/dist/ReactToastify.css";
 
 export const UserContext = createContext(null);
 
@@ -96,27 +55,8 @@ const router = createBrowserRouter([
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
         <NavBar />
         <RouterProvider router={router} />
-=======
-    <NavBar />
-    <BrowserRouter> 
-      <Routes>
-        <Route path="/" element={ <Home />} />
-        <Route path="/services" element={ <Services /> } />
-        <Route path="/login" element={ <Login /> } />
-        <Route path="/signup" element={ <SignUp /> } />
-
-        <Route path="/map" element={ <HereMap /> } />
-        <Route path="/displayallvenues" element={ <DisplayVenues /> } />
-        <Route path="/loader" element={ <Animations /> } />
-        <Route path="/venuecard" element={ <VenueCard /> } />
-        <Route path="/*" element={ <Error /> } />
-
-      </Routes>
-    </BrowserRouter>
->>>>>>> c609266b7e6193e26e0374c0a9a7323248ca0e72
     </>
   );
 };
